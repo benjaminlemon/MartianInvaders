3321efdd32dbbba774680d365215a7a6321bc89b
naveen <naveenspec@gmail.com>
2024-09-17 12:43:40 -0600

    Player appears and moves

--- Player.h
+++ Player.h
@@ -4,15 +4,14 @@
 #include <SFML/Graphics/VertexArray.hpp>
 #include <SFML/Graphics.hpp>
 
-class player{
+class Player{
     private:
         int health;
         sf::Vector2f position;
         
     public:
         sf::CircleShape shape;
-        player();
-        player(int health, sf::Vector2f position, sf::CircleShape shape);
+        Player();
         void move(float x, float y);
         void shoot();
         void updateHealth();
